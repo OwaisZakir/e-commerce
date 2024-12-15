c56bd8c37bdb9c40806d7468cfc43cc72b87222b
OwaisZakir <owaiszakir88@gmial.com>
2024-12-15 15:02:17 +0500

    Testing

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// let postApi = "https://dummyjson.com/products";
+
+// async function posts() {
+//   let response = await fetch(postApi);
+//   return response.json();
+// }
+// let data = posts();
+
+// data
+//   .then((posts) => {
+//     console.log(posts);
+//   })
+//   .catch((error) => {
+//     console.log("Error:", error);
+//   });
